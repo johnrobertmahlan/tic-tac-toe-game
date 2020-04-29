0d7f43dd1fb88e84efcde622d2cbbfd7309615d6
John Robert Mahlan <johnrobertmahlan@Johns-MacBook-Air.local>
2020-04-28 21:35:07 -0600

    construct game logic

--- js/script.js
+++ js/script.js
@@ -52,20 +52,59 @@ buttonEl.addEventListener('click', init);
 
 
 /* ----- functions ----- */
+init(); // Call this function immediately so that the game is loaded when the page loads
+
 function handleClick(evt) {
-    console.log(evt.target.dataset.index);
+    const selectedIdx = evt.target.dataset.index; // This is basically a number 0-8 (see index.html)
+
+    // If a square is clicked on, we need to update the gameboard state
+    // If square already has a value, don't update it!
+    if(winner || gameboard[selectedIdx]) {
+        return; //quits the function if there is a value for gameboard[selectedIdx] (i.e., if gameboard[selectedIdx] is NOT NULL, then we're not going to run the rest of the code); this prevents us from "overwriting" someone who already clicked on a square
+    }
+    gameboard[selectedIdx] = turn; // the gameboard is an array; the index cited in brackets is the index of the square that was clicked on (evt.target.dataset.index) --> this tells us who "owns" that square by assigning it to a player --> we change the value to the value of the player, essentially
+
+    // We also need to toggle whose turn it is
+    turn *= -1;
+
+    // Check the gameboard to see if there is a winner
+    winner = checkWinner();
+
+    render(); // Here we once again transfer the state of the game to the DOM
 }
 
 function init() /* I don't need access to the event, so to speak, so I don't need a parameter; the information in the event is not important for this function */ {
     // This function will start/initialize the game or reset the game
     winner = false; // Nobody has won at the start of the game
-    turn = 1; // We'll start with Player X's turn first
+    turn = 1; // We'll start with Player X's turn first (see the spanEl.textContent = players[turn] thing below and think more about it)
     gameboard = [null, null, null, null, null, null, null, null, null]; // We want the board to be clean when the game starts or resets, so each square needs to be empty
     render(); // This basically connects the CONTROLLER to the VIEW (i.e., the stuff the player can DO to what the player SEES)
 }
 
 function render() {
+    // transfer the state of our gameboard array to our DOM elements
+    gameboard.forEach(function(value, index) {
+        squareEls[index].textContent = players[value]; // This will return either X or O, since player 1 is set to X and player -1 is set to O;
+    })
+    if(!winner) {
+        spanEl.textContent = players[turn]; // What is happening here? We're passing turn to players? Since turn is set to 1...if I want to get the key in the players object to be equal to 1, then - since turn was set to 1 - this is the same as citing players['1]; NOTE that this also fills the span in our HTML to show us which player's turn it is
+    } else if (winner  === 'T') {
+        alert('Tie game!');
+    } else {
+        alert(`${players[winner]} wins!`);
+    }
     
+}
+
+function checkWinner() {
+    // Compare winCombos to gameboard! --> Check winCombos and compare them to current positions of 1s or -1s inside the gameboard (NOTICE THE DATA-CENTRIC APPROACH: NOT LOOKING FOR Xs and Os)
+    // Whoever is the winner, we return that value: 1 or -1
+    // If no winner and no nulls, there is a tie
+    for(let i=0; i<winCombos.length; i++) {
+        if(Math.abs(gameboard[winCombos[i][0]] + gameboard[winCombos[i][1]] + gameboard[winCombos[i][2]]) === 3) return gameboard[winCombos[i][0]];
+        else if(gameboard.includes(null)) return false;
+        else return 'T';
+    }
 }
 
 
